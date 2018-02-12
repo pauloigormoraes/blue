e320dba1afa7d49a8a41b2833f905588c0be75e6
Paulo Igor Moraes <pauloigormoraes@gmail.com>
2018-02-11 21:53:47 -0400

    construindo

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,6 +3,17 @@ var router = express.Router();
 let connect = require("../schema/data");
 let application = connect.Mongoose.model('application', connect.modelSchema, 'application');
 
+router.get('/index', function(req, res) {
+  application.find({}).lean().exec(
+    function (err, results) {
+      res.render('main', { datas: results });
+    });
+});
+
+router.get('/index/category/education', function(req, res) {
+    res.render('education', );
+});
+
 // router.get('/main', function(req, res) {
 //   application.find({}).lean().exec(
 //     function (err, results) {
--- creep/creep.js
+++ creep/creep.js
@@ -1,40 +1,12 @@
-const promise = require('request-promise');
-const cheerio = require('cheerio');
+var gplay = require('google-play-scraper');
 
-const content = {
-  uri: 'https://www.allexlima.com',
-  transform: function (body) {
-    return cheerio.load(body);
-  }
-};
-
-promise(content)
-  .then(($) => {
-    console.log('---# CONTENT #---');
-    console.log($('.text-justify').text());
-    // console.log($('.person-info').text());
-  })
-  .catch((err) => {
-    console.log('---# ERROR #---');
-    console.log(err);
-  });
+gplay.reviews({
+  appId: 'com.wonder',
+  page: 0,
+  num: 250,
+  sort: gplay.sort.RATING
+}).then(console.log, console.log);
 
 
-
-
-
-
-
-
-
-
-
-  var app = new Application({
-           app_name: title
-         });
-  app.save(function (trash) {
-     if (trash) {
-         console.log("ERRO " + trash.message);
-         return trash; }
-     else { res.send("Check your console!"); }
-  });
+// gplay.app({appId: 'com.wonder'})
+//   .then(console.log, console.log);
