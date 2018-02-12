ae0e83e17725591ca1dc54868019e892d204efca
Paulo Igor Moraes <pauloigormoraes@gmail.com>
2018-02-12 01:15:57 -0400

    comentários capturados

--- creep/creep.js
+++ creep/creep.js
@@ -1,8 +1,16 @@
 var gplay = require('google-play-scraper');
+// gplay.app({appId: 'com.memorado.brain.games'});
+// gplay.search({term: "educação", num: 3, lang: "pt", country: "br"}).then(console.log);
 
 gplay.reviews({
-  appId: 'com.wonder',
-  page: 0,
-  num: 250,
-  sort: gplay.sort.RATING
+  appId: 'co.brainly',
+  lang: 'pt',
+  page: 0
 }).then(console.log, console.log);
+
+// db.play.insert({"info":{"url": "https://play.google.com/store/apps/details?id=co.brainly", "appId": 'co.brainly', "title": 'Brainly - estude com a gente', "score": 4, "price": 0, "free": "true"}});
+// db.play.insert({
+// "category": [
+// ["url": "https://play.google.com/store/apps/details?id=org.khanacademy.android", "appId": 'org.khanacademy.android', "title": 'Khan Academy', "score": 4, "price": 0, "free": "true" ],
+// ["url": "https://play.google.com/store/apps/details?id=co.brainly", "appId": 'co.brainly', "title": 'Brainly - estude com a gente', "score": 4, "price": 0, "free": "true" ],
+// ["url": "https://play.google.com/store/apps/details?id=com.ionicframework.cursosdegraca", "appId": 'com.ionicframework.cursosdegraca', "title": 'Cursos de Graça', "score": 4, "price": 0, "free": "true"]]});
