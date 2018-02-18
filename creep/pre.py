0b8326fd32df1ae11d22d52ecd1b95c3f375a6f2
Paulo Igor Moraes <pauloigormoraes@gmail.com>
2018-02-18 12:06:13 -0400

    estamos indo

--- creep/pre.py
+++ creep/pre.py
@@ -1,6 +1,7 @@
 import csv
 import re as rexpression
 from nltk.corpus import stopwords
+from nltk.stem import rslp
 # nltk.download('stopwords')
 from string import punctuation
 import unicodedata
@@ -10,52 +11,69 @@ reload(sys)
 sys.setdefaultencoding("utf-8")
 
 a__ifile = csv.reader(open('/home/paulomoraes/Projects/lise/creep/data/organized/cat-a.csv', 'r'))
-a__ofile = open('/home/paulomoraes/Projects/lise/creep/data/prepared/t_stopwords/wcat-a.csv', 'w')
+a__ofile = open('/home/paulomoraes/Projects/lise/creep/data/prepared/cat-a.csv', 'w')
 
 b__ifile = csv.reader(open('/home/paulomoraes/Projects/lise/creep/data/organized/cat-b.csv', 'r'))
-b__ofile = open('/home/paulomoraes/Projects/lise/creep/data/prepared/t_stopwords/wcat-b.csv', 'w')
+b__ofile = open('/home/paulomoraes/Projects/lise/creep/data/prepared/cat-b.csv', 'w')
 
 c__ifile = csv.reader(open('/home/paulomoraes/Projects/lise/creep/data/organized/cat-c.csv', 'r'))
-c__ofile = open('/home/paulomoraes/Projects/lise/creep/data/prepared/t_stopwords/wcat-c.csv', 'w')
+c__ofile = open('/home/paulomoraes/Projects/lise/creep/data/prepared/cat-c.csv', 'w')
 
-def rmStopWord(text):
-
+def rm_punctuation(text):
     regex = rexpression.compile('[%s]' % rexpression.escape(punctuation))
     noponctuation = []
     words = text.split()
-
     for word in words:
         ntoken = regex.sub(u'', word)
         if not ntoken == u'':
             noponctuation.append(ntoken)
+    return noponctuation
 
+def rm_stopwords(text):
     swords = stopwords.words('portuguese')
-    nospace = [r for r in noponctuation if r.lower().strip() not in swords]
+    content = [r for r in text if r.lower().strip() not in swords]
+    return content
 
+def tokenize(text):
     tclean = []
-    for word in nospace:
+    for word in text:
         nfkd = unicodedata.normalize('NFKD', word)
         noword = u''.join([r for r in nfkd if not unicodedata.combining(r)])
         # ^[A-Za-z][A-Za-z0-9!@#$%^&*]*$ ou [^a-zA-Z0-9 \\\]
         join = rexpression.sub('[^a-zA-Z \\\]',' ', noword)
         tclean.append(join.lower().strip())
-        tokens = [r for r in tclean if len(r) > 2 and not r.isdigit()]
-        tprepared = ' '.join(tokens)
-
+    tokens = [r for r in tclean if len(r) > 2 and not r.isdigit()]
+    tprepared = ' '.join(tokens)
     return tprepared
 
+def stemming(document):
+    words = document.split()
+    stm = []
+    for word in words:
+        stm.append(rslp.RSLPStemmer().stem(word))
+    return (' '.join(stm))
+
 for row in a__ifile:
-    docw = rmStopWord(unicode(row[0]))
+    docw = rm_punctuation(unicode(row[0]))
+    docw = rm_stopwords(docw)
+    docw = tokenize(docw)
+    docw = stemming(unicode(docw))
     a__ofile.write(docw)
     a__ofile.write('\n')
 
 for row in b__ifile:
-    docw = rmStopWord(unicode(row[0]))
+    docw = rm_punctuation(unicode(row[0]))
+    docw = rm_stopwords(docw)
+    docw = tokenize(docw)
+    docw = stemming(unicode(docw))
     b__ofile.write(docw)
     b__ofile.write('\n')
 
 for row in c__ifile:
-    docw = rmStopWord(unicode(row[0]))
+    docw = rm_punctuation(unicode(row[0]))
+    docw = rm_stopwords(docw)
+    docw = tokenize(docw)
+    docw = stemming(unicode(docw))
     c__ofile.write(docw)
     c__ofile.write('\n')
 
