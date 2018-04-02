34daee43ac5ed292452f05c7ace5223ab13a4899
pauloigormoraes <pauloigormoraes@gmail.com>
2018-04-02 14:18:45 -0400

    Resultados dividos por grupos

--- back/alg/kmeans.py
+++ back/alg/kmeans.py
@@ -2,57 +2,116 @@
 import csv
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.cluster import KMeans
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import tempfile
 import numpy as np
 
 def main():
 
-    list_rw = open('/home/paulomoraes/Projects/blueway/back/dataset/data_clean.csv', 'r')
+    list_rw = open('/home/paulomoraes/Projects/blue/back/dataset/data_clean.csv', 'r')
+    aux = open('/home/paulomoraes/Projects/blue/back/dataset/reviews.csv', 'r')
     arr = list_rw.read().strip().split('\n')
+    reviews = aux.read().strip().split('\n')
+
+    group_a_file = open('/home/paulomoraes/Projects/blue/back/dataset/group_a.csv', 'a')
+    group_b_file = open('/home/paulomoraes/Projects/blue/back/dataset/group_b.csv', 'a')
+    group_c_file = open('/home/paulomoraes/Projects/blue/back/dataset/group_c.csv', 'a')
+    group_d_file = open('/home/paulomoraes/Projects/blue/back/dataset/group_d.csv', 'a')
+
+    values_np = np.array(arr)
+    # print(values_clnp)
 
     vectorizer = TfidfVectorizer(use_idf=True)
-    bag_of_words = vectorizer.fit_transform(arr)
+    bag_of_words = vectorizer.fit_transform(values_np)
+    # print(bag_of_words)
 
     # vectorizer = CountVectorizer(max_df=0.95, min_df=5)
     # bag_of_words = vectorizer.fit_transform(arr)
 
-    for i in bag_of_words:
-        for j in i:
-            print(j)
+    for r in range(10):
+        kmeans = KMeans(n_clusters = 4).fit(bag_of_words)
+    # print(kmeans.cluster_centers_)
 
-    # kmeans = KMeans(n_clusters = 5, init = 'random')
-    # kmeans.fit(bag_of_words)
-    # print(kmeans.labels_)
-
-    # distance = kmeans.fit_transform(bag_of_words)
+    distance = kmeans.fit_transform(bag_of_words)
     # print(distance)
 
-    # labels = kmeans.labels_
+    labels = kmeans.labels_
     # print(labels)
 
+    # Onde cada instância vai ser clusterizada, i. e., em qual grupo cada review será agrupado;
+    predict = kmeans.predict(bag_of_words)
+
     # wcss = []
-    # for r in range(1, 21):
+    # for r in range(1, 11):
     #     kmeans = KMeans(n_clusters = r, init = 'random')
     #     kmeans.fit(bag_of_words)
     #     print(r, kmeans.inertia_)
     #     wcss.append(kmeans.inertia_)
-    # plt.plot(range(1, 21), wcss)
-    # plt.title('ELBOW METHOD')
+    # plt.plot(range(1, 11), wcss)
+    # plt.title('Método Elbow')
     # plt.xlabel('No Clusters')
-    # plt.ylabel('DB')
+    # plt.ylabel('Base de Dados')
     # plt.show()
 
-    # plt.scatter(distance[:, 0], distance[:,1], s = 50, c = kmeans.labels_)
+    # plt.scatter(distance[:, 0], distance[:,1], s = 100, c = kmeans.labels_)
     # # plt.scatter(distance[:, 0], distance[:,1], marker='x', s=50, linewidths=3, c=kmeans.labels_)
-    # plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 50, color = 'gray', label = 'Centroids')
+    # plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 300, color = 'red', label = 'Centroids')
     # plt.title('Reviews Clusters and Centroids')
     # plt.xlabel('X')
     # plt.ylabel('Y')
     # plt.legend()
     # plt.show()
 
+    print()
+    print("***** ---- --- ---- *******")
+    print()
+
+    results = []
+    group_a = []
+    group_b = []
+    group_c = []
+    group_d = []
+
+    for ix_pdc, value in enumerate(predict):
+        for ix_rvw, content in enumerate(reviews):
+            if ix_pdc == ix_rvw:
+                if value == 0:
+                    a = [value, content]
+                    group_a.append(a)
+                    group_a_file.write(content)
+                    group_a_file.write('\n')
+                    group_a_file.close
+                elif value == 1:
+                    b = [value, content]
+                    group_b.append(b)
+                    group_b_file.write(content)
+                    group_b_file.write('\n')
+                    group_b_file.close
+                elif value == 2:
+                    c = [value, content]
+                    group_c.append(c)
+                    group_c_file.write(content)
+                    group_c_file.write('\n')
+                    group_c_file.close
+                elif value == 3:
+                    d = [value, content]
+                    group_d.append(d)
+                    group_d_file.write(content)
+                    group_d_file.write('\n')
+                    group_d_file.close
+                aux = [value, content]
+                results.append(aux)
+
+    print(len(group_a))
+    print(len(group_b))
+    print(len(group_c))
+    print(len(group_d))
+
+    print(len(group_a) + len(group_b) + len(group_c) + len(group_d))
+
+    print()
     print("***** ---- END ---- *******")
+    print()
 
 if __name__ == "__main__":
     main()
